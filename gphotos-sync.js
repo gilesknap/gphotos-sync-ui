5c5bad200a884e7b29d2a4b3f76e56cc64a3cd8c
Open e-ID <open.eid@outlook.com>
2019-11-14 03:30:03 +0100

    Find the tools

--- gphotos-sync.js
+++ gphotos-sync.js
@@ -9,11 +9,47 @@ const { exec, execSync } = require('child_process');
 try {
 
   let mainWindow;
+  var pip = '';
+  var tool = '';
   
   ipcMain.on('test', function(e, message) {
     e.sender.send('test', '');
   });
-
+  
+  // find tool
+  locate(['gphotos-sync', 'gphotos-sync.*'], function(result) {
+    tool = result;
+    if(tool == '') {
+      // find pip
+      locate(['pip', 'pip.exe'], function(result) {
+        pip = result;
+        // find python
+        if(pip == '') {
+          locate(['python3', 'python*.exe'], function(result) {
+            pip = result;
+            if(pip != '') pip += ' -m pip';   
+            about_tools();         
+          });
+        } else {
+          about_tools();
+        }
+      });    
+    }   
+  });
+  
+  function about_tools() {
+    fs.writeFileSync(path.join(os.homedir(), 'gPhotos Sync.log'), JSON.stringify([tool, pip]));    
+    if(pip == '') {
+      dialog.showErrorBox('Error', 'python/pip not installed');
+    } else {
+      run(pip + ' show gphotos-sync', function(result) {
+        if(result != '') tool = pip + ' run gphotos-sync';
+        if(tool == '') dialog.showErrorBox('Error', 'gphotos-sync not installed');
+        dialog.showErrorBox('Info', 'gphotos-sync installed @' + tool + ', pip installed @' + pip);    
+      });
+    }
+  }
+  
   app.once('ready', () => {
       mainWindow = new BrowserWindow({
           title: 'gPhotos Sync',
@@ -47,7 +83,48 @@ try {
           app.quit();
       });        
   });     	  	
-  	
+  
+  function locate(path, callback) {
+    var where = 'which ' + path[0];
+    if(os.platform().indexOf('win') == 0) where = 'where /r "%ProgramFiles%" ' + path[1]; // ???
+    var found = [''];
+    try {
+      run(where, function(result) {
+        found = result.replace(/\r/g, '').split('\n');
+        for(var i in found) {
+          if(found[i] != '') {
+            callback(found[i]);
+            return;
+          }
+        }
+        callback('');    
+      });
+    } catch(e) {
+    }
+  }
+  
+  function run(cmd, callback) {
+    var spawn = require('child_process').spawn;
+    
+    //kick off process of listing files
+    args = cmd.split(' ');
+    var proc = args[0];
+    delete args[0];
+    var child = spawn(proc, args);
+    
+    var result = '';
+    
+    //spit stdout to screen
+    child.stdout.on('data', function (data) {   process.stdout.write(data.toString()); result += data.toString(); });
+    
+    //spit stderr to screen
+    child.stderr.on('data', function (data) {   process.stdout.write(data.toString()); result += data.toString(); });
+    
+    child.on('close', function (code) { 
+        console.log("Finished with code " + code);
+        callback(result);
+    });    
+  }	
   
 } catch(e){
     dialog.showErrorBox('Error', e.stack);
