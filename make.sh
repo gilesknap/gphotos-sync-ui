290284352fc9d3fdbd801ec3cae0b7b69ec9f1f1
michael79bxl <michael@barchy.be>
2019-11-26 00:27:11 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -10,26 +10,27 @@ then
   curl -L https://github.com/patr0nus/DeskGap/releases/download/v0.1.0/deskgap-v0.1.0-linux-x64.zip --output deskgap.zip
   unzip deskgap.zip
   rm deskgap.zip
-  rm -f resources/app/*
   mv "./DeskGap" "./gPhotos Sync"
-  chmod +x "./gPhotos Sync"
+  rm -f "/gPhotos Sync/resources/app/*"
+  mv "./DeskGap/DeskGap" "./gPhotos Sync"
+  chmod +x "./gPhotos Sync/gPhotos Sync"
   cd ..
 fi
 echo Building gPhotos Sync App...
-cp -f package.json "./linux/resources/app/package.json"
-cp -f material.min.css "./linux/resources/app/material.min.css"
-cp -f material.min.js "./linux/resources/app/material.min.js"
-cp -f material.min.css.map "./linux/resources/app/material.min.css.map"
-cp -f material.min.js.map "./linux/resources/app/material.min.js.map"
-cp -f dialog-polyfill.js "./linux/resources/app/dialog-polyfill.js"
-cp -f dialog-polyfill.css "./linux/resources/app/dialog-polyfill.css"
-cp -f icons.woff2 "./linux/resources/app/icons.woff2"
-cp -f sql.js "./linux/resources/app/sql.js"
-cp -f gphotos-sync.js "./linux/resources/app/gphotos-sync.js"
-cp -f gphotos-sync-ui.js "./linux/resources/app/gphotos-sync-ui.js"
-cp -f gphotos-sync.css "./linux/resources/app/gphotos-sync.css"
-cp -f gphotos-sync.html "./linux/resources/app/gphotos-sync.html"
-cp -f gphotos.html "./linux/resources/app/gphotos.html"
+cp -f package.json "./linux/gPhoto Sync/resources/app/package.json"
+cp -f material.min.css "./linux/gPhoto Sync/resources/app/material.min.css"
+cp -f material.min.js "./linux/gPhoto Sync/resources/app/material.min.js"
+cp -f material.min.css.map "./linux/gPhoto Sync/resources/app/material.min.css.map"
+cp -f material.min.js.map "./linux/gPhoto Sync/resources/app/material.min.js.map"
+cp -f dialog-polyfill.js "./linux/gPhoto Sync/resources/app/dialog-polyfill.js"
+cp -f dialog-polyfill.css "./linux/gPhoto Sync/resources/app/dialog-polyfill.css"
+cp -f icons.woff2 "./linux/gPhoto Sync/resources/app/icons.woff2"
+cp -f sql.js "./linux/gPhoto Sync/resources/app/sql.js"
+cp -f gphotos-sync.js "./linux/gPhoto Sync/resources/app/gphotos-sync.js"
+cp -f gphotos-sync-ui.js "./linux/gPhoto Sync/resources/app/gphotos-sync-ui.js"
+cp -f gphotos-sync.css "./linux/gPhoto Sync/resources/app/gphotos-sync.css"
+cp -f gphotos-sync.html "./linux/gPhoto Sync/resources/app/gphotos-sync.html"
+cp -f gphotos.html "./linux/gPhoto Sync/resources/app/gphotos.html"
 cd linux
 zip -r "../../gPhoto Sync.zip" *
 echo Done.
