0a80899cb493494530a9c77b657f97b6d3f90216
michael79bxl <michael@barchy.be>
2019-11-26 01:02:17 +0100

    Update make.cmd

--- make.cmd
+++ make.cmd
@@ -12,20 +12,20 @@ if not exist "win" (
   cd ..
 )
 echo Building gPhotos Sync App...
-cp -f package.json ".\win\gPhotos Sync\resources\app\package.json"
-cp -f material.min.css ".\win\gPhotos Sync\resources\app\material.min.css"
-cp -f material.min.js ".\win\gPhotos Sync\resources\app\material.min.js"
-cp -f material.min.css.map ".\win\gPhotos Sync\resources\app\material.min.css.map"
-cp -f material.min.js.map ".\win\gPhotos Sync\resources\app\material.min.js.map"
-cp -f dialog-polyfill.js ".\win\gPhotos Sync\resources\app\dialog-polyfill.js"
-cp -f dialog-polyfill.css ".\win\gPhotos Sync\resources\app\dialog-polyfill.css"
-cp -f icons.woff2 ".\win\gPhotos Sync\resources\app\icons.woff2"
-cp -f sql.js ".\win\gPhotos Sync\resources\app\sql.js"
-cp -f gphotos-sync.js ".\win\gPhotos Sync\resources\app\gphotos-sync.js"
-cp -f gphotos-sync-ui.js ".\win\gPhotos Sync\resources\app\gphotos-sync-ui.js"
-cp -f gphotos-sync.css ".\win\gPhotos Sync\resources\app\gphotos-sync.css"
-cp -f gphotos-sync.html ".\win\gPhotos Sync\resources\app\gphotos-sync.html"
-cp -f gphotos.html ".\win\gPhotos Sync\resources\app\gphotos.html"
+copy /y package.json ".\win\gPhotos Sync\resources\app\package.json"
+copy /y material.min.css ".\win\gPhotos Sync\resources\app\material.min.css"
+copy /y material.min.js ".\win\gPhotos Sync\resources\app\material.min.js"
+copy /y material.min.css.map ".\win\gPhotos Sync\resources\app\material.min.css.map"
+copy /y material.min.js.map ".\win\gPhotos Sync\resources\app\material.min.js.map"
+copy /y dialog-polyfill.js ".\win\gPhotos Sync\resources\app\dialog-polyfill.js"
+copy /y dialog-polyfill.css ".\win\gPhotos Sync\resources\app\dialog-polyfill.css"
+copy /y icons.woff2 ".\win\gPhotos Sync\resources\app\icons.woff2"
+copy /y sql.js ".\win\gPhotos Sync\resources\app\sql.js"
+copy /y gphotos-sync.js ".\win\gPhotos Sync\resources\app\gphotos-sync.js"
+copy /y gphotos-sync-ui.js ".\win\gPhotos Sync\resources\app\gphotos-sync-ui.js"
+copy /y gphotos-sync.css ".\win\gPhotos Sync\resources\app\gphotos-sync.css"
+copy /y gphotos-sync.html ".\win\gPhotos Sync\resources\app\gphotos-sync.html"
+copy /y gphotos.html ".\win\gPhotos Sync\resources\app\gphotos.html"
 echo Done.
 rem *** TODO: icon, pack ***
 pause
